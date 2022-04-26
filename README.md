7be9b772050350d69600e8fd9b895b5555d4f08a
pawel <pawelgagorowski026@gmail.com>
2022-04-26 11:41:46 +0200

    feat(chore) add description to README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # localvoice-api
+
+This is the api for SaaS Platform of the Localvoice application that allows you to create voicebots and chatbots - perfect for tutors, language schools and influencers
--- publish.sh
+++
@@ -1,3 +0,0 @@
-rm -fr lambda_upload.zip
-zip -r lambda_upload.zip api/action-to-readFunction.js utils/intent.js
-aws lambda update-function-code --function-name ga-web-english-project-prod-put-text-to-readFunction --zip-file fileb://lambda_upload.zip
